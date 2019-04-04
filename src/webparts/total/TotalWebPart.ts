12328e1f6be9697652a9096d6d6091146156df5f
kyleschaeffer <kyle@spirito.us>
2019-04-04 15:03:09 -0400

    Add dynamic dat field

--- src/webparts/total/TotalWebPart.ts
+++ src/webparts/total/TotalWebPart.ts
@@ -1,20 +1,32 @@
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IPropertyPaneConfiguration, PropertyPaneSlider } from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart, IWebPartPropertiesMetadata } from '@microsoft/sp-webpart-base';
+import { IPropertyPaneConfiguration, PropertyPaneSlider, IPropertyPaneConditionalGroup, PropertyPaneDynamicFieldSet, PropertyPaneDynamicField } from '@microsoft/sp-property-pane';
 import { Total, ITotalProps } from './components/Total';
 import { Version } from '@microsoft/sp-core-library';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import { DynamicProperty } from '@microsoft/sp-component-base';
 
 export interface ITotalWebPartProps {
-  total: number;
+  total: DynamicProperty<number>;
 }
 
 export default class TotalWebPart extends BaseClientSideWebPart<ITotalWebPartProps> {
   public render(): void {
+    // Get dynamic props
+    const total: number | undefined = this.properties.total.tryGetValue();
+
     const element: React.ReactElement<ITotalProps> = React.createElement(Total, {
-      total: this.properties.total,
+      total,
     });
     ReactDom.render(element, this.domElement);
+  }
+
+  protected get propertiesMetadata(): IWebPartPropertiesMetadata {
+    return {
+      total: {
+        dynamicPropertyType: 'number',
+      },
+    };
   }
 
   protected onDispose(): void {
@@ -34,15 +46,31 @@ export default class TotalWebPart extends BaseClientSideWebPart<ITotalWebPartPro
           },
           groups: [
             {
-              groupName: 'Value',
-              groupFields: [
-                PropertyPaneSlider('total', {
-                  label: 'Total Value',
-                  min: 0,
-                  max: 10000,
-                }),
-              ]
-            }
+              primaryGroup: {
+                groupName: 'Value',
+                groupFields: [
+                  PropertyPaneSlider('total', {
+                    label: 'Total Value',
+                    min: 0,
+                    max: 10000,
+                  }),
+                ]
+              },
+              secondaryGroup: {
+                groupName: 'Value',
+                groupFields: [
+                  PropertyPaneDynamicFieldSet({
+                    label: 'Total',
+                    fields: [
+                      PropertyPaneDynamicField('total', {
+                        label: 'Total Value',
+                      }),
+                    ]
+                  })
+                ]
+              },
+              showSecondaryGroup: !!this.properties.total.tryGetSource(),
+            } as IPropertyPaneConditionalGroup
           ]
         }
       ]
